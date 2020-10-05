9e8ccb06432fb9f0edf8e23ea633e2034b1a0ef8
aholtzman <adam.holtzman@gmail.com>
2020-10-04 21:04:27 -0400

    init

---
+++ src/templates/blog-template.js
@@ -0,0 +1,43 @@
+import { html, LitElement } from 'lit-element';
+import '@evergreen-wc/eve-container';
+import '../components/footer/footer';
+import '../components/header/header';
+import '../styles/theme.css';
+import blogCss from '../styles/blog.css';
+
+class BlogTemplate extends LitElement {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <style>
+        ${blogCss}
+      </style>
+
+      <div class='wrapper'>
+        <app-header></app-header>
+
+        <eve-container fluid>
+          <div class='page-template content'>            
+            <entry></entry>
+
+            <h3>More Posts</h3>
+            <nav>
+              <ul>
+                <li><a href="/blog/second-post">My Second Post</a></li>
+                <li><a href="/blog/first-post">My First Post</a></li>
+              </ul>
+            </nav>
+          </div>
+        </eve-container>
+
+        <app-footer></app-footer>
+      </div>
+    `;
+  }
+}
+
+customElements.define('page-template', BlogTemplate);
